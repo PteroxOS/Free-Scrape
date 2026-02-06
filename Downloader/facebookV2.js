bce9efa860d92b5fe657198443c67fbbf9f1fa26
Agung Mahesa <agungdevx@gmail.com>
2026-02-06 13:42:12 +0700

    Update facebookV2.js

--- Downloader/facebookV2.js
+++ Downloader/facebookV2.js
@@ -38,14 +38,14 @@ async function fbDown(url) {
 
         if (!hd && !sd) {
             return {
-                creator: "@ShiroNexo",
+                creator: "AgungDevX",
                 status: false,
                 message: "Video teu kapanggih. Facebook keur proteksi, coba ganti IP/VPN."
             };
         }
 
         return {
-            creator: "@ShiroNexo",
+            creator: "AgungDevX",
             status: true,
             title,
             hd,
@@ -54,7 +54,7 @@ async function fbDown(url) {
 
     } catch (err) {
         return {
-            creator: "@ShiroNexo",
+            creator: "AgungDevX",
             status: false,
             message: err.response ? `Error ${err.response.status}: Facebook nolak request.` : err.message
         };
@@ -72,7 +72,7 @@ fbDown(tester).then(res => {
 // Conto hasilna
 /**
 {
-  "creator": "@ShiroNexo",
+  "creator": "AgungDevX",
   "status": true,
   "title": "Facebook",
   "hd": "https://video.fbdo18-1.fna.fbcdn.net/o1/v/t2/f2/m86/AQMdg8K5f_DAf4Y6_rD1zBQ1ssEOU0R6mIJu3cTRdnd6hT0lo0JI5K38kOGrIICCnQ52Fu7g214eW7_S8opDzUJILdbHlqJbOd7YRrs.mp4?_nc_cat=109&_nc_oc=AdlhHHLi7LSm1nAi7nZLv-XgP67ndB91aX2DxAHy6YBtLDAtUhel5LUOY_rqUO6Qe5E&_nc_sid=5e9851&_nc_ht=video.fbdo18-1.fna.fbcdn.net&_nc_ohc=7YPLJH4CXXEQ7kNvwFERblR&efg=eyJ2ZW5jb2RlX3RhZyI6Inhwdl9wcm9ncmVzc2l2ZS5GQUNFQk9PSy4uQzMuNzIwLmRhc2hfYmFzZWxpbmVfMV92MSIsInhwdl9hc3NldF9pZCI6MTc5MzgzMDIzOTkxMTk4NzksImFzc2V0X2FnZV9kYXlzIjozLCJ2aV91c2VjYXNlX2lkIjoxMDA5OSwiZHVyYXRpb25fcyI6MTksInVybGdlbl9zb3VyY2UiOiJ3d3cifQ%3D%3D&ccb=17-1&vs=97762bb71e7d70e&_nc_vs=HBksFQIYUmlnX3hwdl9yZWVsc19wZXJtYW5lbnRfc3JfcHJvZC85MzRDNTExRDc5MTgxQjk3RDZDRTcxM0Q0MTc2NjlCQV92aWRlb19kYXNoaW5pdC5tcDQVAALIARIAFQIYUWlnX3hwdl9wbGFjZW1lbnRfcGVybWFuZW50X3YyLzM0NDczQjU5NDMzQTQ1REFCNzdDRkYzODNGMDEzNUFDX2F1ZGlvX2Rhc2hpbml0Lm1wNBUCAsgBEgAoABgAGwKIB3VzZV9vaWwBMRJwcm9ncmVzc2l2ZV9yZWNpcGUBMRUAACaOiPzlz7LdPxUCKAJDMywXQDNEGJN0vGoYEmRhc2hfYmFzZWxpbmVfMV92MREAdQJl5p0BAA&_nc_gid=_6rqMp_8jjayMugkc5rOaw&_nc_zt=28&oh=00_AfuXycdSoDx41BtxBbhPUNcGEdUbXyK6DoDYWnsUz7jryg&oe=69876BD2&bitrate=2713258&tag=dash_baseline_1_v1",
